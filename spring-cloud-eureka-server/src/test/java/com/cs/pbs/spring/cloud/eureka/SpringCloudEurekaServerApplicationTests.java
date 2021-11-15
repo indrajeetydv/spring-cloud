01bb5d9051af7d5cd805c261ab647446e30e008d
indrajeet yadav <dev.indrajeetydv@gmail.com>
2021-11-15 16:57:50 +0530

    spring cloud discovery registry

---
+++ spring-cloud-eureka-server/src/test/java/com/cs/pbs/spring/cloud/eureka/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.pbs.spring.cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
